c18236d5e0b63779fafbc08ae2a24137dbe956dc
totolacky <totolacky@kaist.ac.kr>
2020-07-05 21:57:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ args = dotdict({
 
     'checkpoint': './temp/',
     'load_model': True,
-    'load_folder_file': ('./temp/','checkpoint_0.pth.tar'),
+    'load_folder_file': ('./temp/','temp.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
 })
