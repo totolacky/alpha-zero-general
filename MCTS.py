63b7cfaeb2414b79a6b12def0bea30f1894ac5a5
totolacky <totolacky@kaist.ac.kr>
2020-07-05 16:38:14 +0900

    Fix bug

--- MCTS.py
+++ MCTS.py
@@ -85,7 +85,7 @@ class MCTS():
 
         if s not in self.Ps:
             # leaf node
-            self.Ps[s], v = self.nnet.predict(canonicalBoard)
+            self.Ps[s], v = self.nnet.predict(canonicalBoard[0])
             valids = self.game.getValidMoves(canonicalBoard, 1)
             self.Ps[s] = self.Ps[s] * valids  # masking invalid moves
             sum_Ps_s = np.sum(self.Ps[s])
